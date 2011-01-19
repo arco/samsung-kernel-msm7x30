14dd3da0bf90c32458de722506cb55e9aa674c35
Benoit Goby <benoit@android.com>
2011-01-19 14:10:37 -0800

    input: evdev: Add missing wake_lock_destroy
    Change-Id: Ief1ef44d98a197e5b457f5e8617c413e76e3c6d2
    Signed-off-by: Benoit Goby <benoit@android.com>

--- drivers/input/evdev.c
+++ drivers/input/evdev.c
@@ -296,6 +296,7 @@ static int evdev_open(struct inode *inode, struct file *file)
  err_free_client:
 	wake_lock_destroy(&client->wake_lock);
 	evdev_detach_client(evdev, client);
+	wake_lock_destroy(&client->wake_lock);
 	kfree(client);
  err_put_evdev:
 	put_device(&evdev->dev);
