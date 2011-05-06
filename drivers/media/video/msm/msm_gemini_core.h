9d1daa684ee4291b40b445a8328c57adb04dc25d
Sunid Wilson <sunidw@codeaurora.org>
2011-05-05 20:59:18 -0700

    msm: gemini: Fix gemini freeze issue if output size estimation is improper
    Update the pong address to fix gemini freeze issue when output size
    estimation is improper.
    
    Change-Id: Ief02e8b326e350d21af5c777cf411a6ceb033bd8
    Signed-off-by: Sunid Wilson <sunidw@codeaurora.org>

--- drivers/media/video/msm/msm_gemini_core.h
+++ drivers/media/video/msm/msm_gemini_core.h
@@ -46,6 +46,6 @@ int msm_gemini_core_we_buf_reset(struct msm_gemini_hw_buf *buf);
 int msm_gemini_core_reset(uint8_t op_mode, void *base, int size);
 int msm_gemini_core_fe_start(void);
 
-void msm_gemini_core_release(void);
+void msm_gemini_core_release(int);
 void msm_gemini_core_init(void);
 #endif /* MSM_GEMINI_CORE_H */
