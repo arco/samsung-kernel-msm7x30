e697e1050818f93ec7f5a2eba496086bb4698700
Arne Coucheron <arco68@gmail.com>
2011-11-18 01:49:56 +0100

    ancora: Disable some debugging spam in kernel log

--- drivers/misc/fsa9480.c
+++ drivers/misc/fsa9480.c
@@ -43,7 +43,7 @@
 #include <asm/io.h>
 #include <linux/uaccess.h>
 
-#define DEBUG 1
+// #define DEBUG 1
 #include <linux/device.h>
 
 #if defined(CONFIG_MACH_ARIESVE) || defined(CONFIG_MACH_ANCORA) || defined(CONFIG_MACH_GODART) || defined(CONFIG_MACH_ANCORA_TMO) || defined(CONFIG_MACH_APACHE)
