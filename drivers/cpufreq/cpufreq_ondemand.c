e57952cb35f368d7cb0daa93f36fe602e2c1939a
Narayanan Gopalakrishnan <nargop@codeaurora.org>
2012-06-26 14:28:32 -0700

    cpufreq: fix utilization statistics for sync cpus
    If the cpus are using related_cpus policy, then the
    current implementaion will overwrite the the calculated
    load for every iteration in the for loop. Hence, mpdecision
    will read incorrect value, if the cores are using related
    cpus policy. With this change mpdecision will read the
    sum of the loads of all cpu's, if related cpu policy is
    used.
    
    Change-Id: I242c8fe39fd75dc3e48750a5b78a5d218b536ddd
    Signed-off-by: Narayanan Gopalakrishnan <nargop@codeaurora.org>
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -857,6 +857,10 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		load_freq = cur_load * freq_avg;
 		if (load_freq > max_load_freq)
 			max_load_freq = load_freq;
+
+		/* calculate the scaled load across CPU */
+		load_at_max_freq += (cur_load * policy->cur) /
+					policy->cpuinfo.max_freq;
 	}
 
 	for_each_online_cpu(j) {
@@ -884,9 +888,6 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 				dbs_tuners_ins.up_threshold_any_cpu_load;
 		}
 	}
-
-	/* calculate the scaled load across CPU */
-	load_at_max_freq = (cur_load * policy->cur)/policy->cpuinfo.max_freq;
 
 	cpufreq_notify_utilization(policy, load_at_max_freq);
 	/* Check for frequency increase */
