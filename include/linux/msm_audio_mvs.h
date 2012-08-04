28e5bcd67811e412c0001af436da4fcfb07aca6b
Arne Coucheron <arco68@gmail.com>
2012-08-04 06:03:25 +0200

    Revert "msm: qdsp5v2: Add support for variable rate in MVS driver"
    This reverts commit 3d0758eae98eb161de3991ff3c7e70023613824d.

--- include/linux/msm_audio_mvs.h
+++ include/linux/msm_audio_mvs.h
@@ -120,15 +120,9 @@ enum msm_audio_g729a_frame_type {
 	MVS_G729A_ERASURE
 };
 
-struct min_max_rate {
-	uint32_t min_rate;
-	uint32_t max_rate;
-};
-
 struct msm_audio_mvs_config {
 	uint32_t mvs_mode;
 	uint32_t rate_type;
-	struct min_max_rate min_max_rate;
 	uint32_t dtx_mode;
 };
 
