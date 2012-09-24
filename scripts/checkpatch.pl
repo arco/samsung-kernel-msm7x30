f4586fba510b9c170a715f57d05ccfd4427bbb7f
Arne Coucheron <arco68@gmail.com>
2012-09-25 01:31:43 +0200

    Revert "semaphore: Remove mutex emulation"
    This reverts commit 4882720b267b7b1d1b0ce08334b205f0329d4615.

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2981,8 +2981,12 @@ sub process {
 			WARN("__func__ should be used instead of gcc specific __FUNCTION__\n"  . $herecurr);
 		}
 
-# check for semaphores initialized locked
-		if ($line =~ /^.\s*sema_init.+,\W?0\W?\)/) {
+# check for semaphores used as mutexes
+		if ($line =~ /^.\s*(DECLARE_MUTEX|init_MUTEX)\s*\(/) {
+			WARN("mutexes are preferred for single holder semaphores\n" . $herecurr);
+		}
+# check for semaphores used as mutexes
+		if ($line =~ /^.\s*init_MUTEX_LOCKED\s*\(/) {
 			WARN("consider using a completion\n" . $herecurr);
 
 		}
