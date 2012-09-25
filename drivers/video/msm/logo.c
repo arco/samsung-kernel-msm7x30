dd4126e5021a63b6dc9256cd781cdacc2be938ee
Arne Coucheron <arco68@gmail.com>
2012-09-25 02:24:33 +0200

    ancora: Bring in video changes from old kernel

--- drivers/video/msm/logo.c
+++ drivers/video/msm/logo.c
@@ -36,6 +36,127 @@ static void memset16(void *_ptr, unsigned short val, unsigned count)
 		*ptr++ = val;
 }
 
+/* convert RGB565 to RBG8888 */
+static void memset16_rgb8888(void *_ptr, unsigned short val, unsigned count)
+{
+	unsigned short *ptr = _ptr;
+	unsigned short red;
+	unsigned short green;
+	unsigned short blue;
+
+	red = ( val & 0xF800) >> 8;
+	green = (val & 0x7E0) >> 3;
+	blue = (val & 0x1F) << 3;
+
+	count >>= 1;
+	while (count--)
+	{
+		*ptr++ = (red<<8) | green;
+		*ptr++ = blue;
+	}
+}
+
+/* 565RLE image format: [count(2 bytes), rle(2 bytes)] */
+#ifdef CONFIG_FB_MSM_SEC_BOOTLOGO
+int load_565rle_image_onfb( char *filename, int start_x, int start_y)
+{
+	int fd, err = 0;
+	unsigned count, max;
+	unsigned short *data, *bits, *ptr;
+	struct fb_info *info;
+	unsigned short *p_line;
+	unsigned short sp, c_pixel;
+	unsigned draw_pixel;
+#ifndef CONFIG_FRAMEBUFFER_CONSOLE
+	struct module *owner;
+#endif
+	info = registered_fb[0];
+
+	if (!info) {
+		printk(KERN_WARNING "%s: Can not access framebuffer\n",
+			__func__);
+		return -ENODEV;
+	}
+#ifndef CONFIG_FRAMEBUFFER_CONSOLE
+	owner = info->fbops->owner;
+	if (!try_module_get(owner))
+		return NULL;
+	if (info->fbops->fb_open && info->fbops->fb_open(info, 0)) {
+		module_put(owner);
+		return NULL;
+	}
+#endif
+	fd = sys_open(filename, O_RDONLY, 0);
+	if (fd < 0) {
+		printk(KERN_WARNING "%s: Can not open %s\n",
+			__func__, filename);
+		return -ENOENT;
+	}
+	count = (unsigned)sys_lseek(fd, (off_t)0, 2);
+	if (count == 0) {
+		sys_close(fd);
+		err = -EIO;
+		goto err_logo_close_file;
+	}
+	sys_lseek(fd, (off_t)0, 0);
+	data = kmalloc(count, GFP_KERNEL);
+	if (!data) {
+		printk(KERN_WARNING "%s: Can not alloc data\n", __func__);
+		err = -ENOMEM;
+		goto err_logo_close_file;
+	}
+	if ((unsigned)sys_read(fd, (char *)data, count) != count) {
+		err = -EIO;
+		goto err_logo_free_data;
+	}
+
+	max = fb_width(info) * fb_height(info);
+
+	ptr = data;
+	bits = (unsigned short *)(info->screen_base+(info->fix.line_length*start_y) );
+
+	p_line = bits;
+	sp = start_x; // start pixel in Line
+	while (count > 3) {
+		unsigned n = ptr[0];  // pixel count
+
+		if (n > max)
+			break;
+		draw_pixel = n;
+		while(draw_pixel > 0)
+		{
+			if(sp == 480)
+			{
+				// move next-line
+				p_line += (info->fix.line_length / 2);
+				sp = 0;
+			}
+
+			if( (sp + draw_pixel) >= 480)
+				c_pixel = 480 - sp;
+			else
+				c_pixel = draw_pixel;
+
+			memset16_rgb8888(&p_line[sp*2], ptr[1], c_pixel<<1);
+
+			draw_pixel -= c_pixel; // decrease drawed-pixel-count
+			sp += c_pixel;
+		}
+
+		max -= n;
+		ptr += 2;
+		count -= 4;
+	}
+
+err_logo_free_data:
+	kfree(data);
+err_logo_close_file:
+	sys_close(fd);
+	return err;
+}
+EXPORT_SYMBOL(load_565rle_image_onfb);
+#endif
+
 /* 565RLE image format: [count(2 bytes), rle(2 bytes)] */
 int load_565rle_image(char *filename, bool bf_supported)
 {
