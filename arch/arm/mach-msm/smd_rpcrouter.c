bc695be83a99aebcff93fbdc974598aae7140e41
Arun Kumar Neelakantam <aneela@codeaurora.org>
2012-10-16 23:13:09 +0530

    msm: rpc: add shutdown and restart handler for rpc
    Add shutdown and restart handler for rpcrouter. The
    shutdown handler take care of calling the
    msm_rpcrouter_close function with a pre defined
    priority. PM no longer handle rpc close in the shutdown/reboot
    sequence.
    
    CRs-fixed: 396641
    Signed-off-by: Arun Kumar Neelakantam <aneela@codeaurora.org>
    Signed-off-by: Angshuman Sarkar <angshuman@codeaurora.org>
    (cherry picked from commit 0975c60881a90c0adf36bac4739b812e34acb813)
    Change-Id: I3dd21a72eb624273b16f5a300b55b068a02d509b
    Signed-off-by: Gopal Goberu <ggober@codeaurora.org>

--- arch/arm/mach-msm/smd_rpcrouter.c
+++ arch/arm/mach-msm/smd_rpcrouter.c
@@ -1,7 +1,7 @@
 /* arch/arm/mach-msm/smd_rpcrouter.c
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2007-2011, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2007-2012, The Linux Foundation. All rights reserved.
  * Author: San Mehat <san@android.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -162,6 +162,7 @@ static atomic_t pm_mid = ATOMIC_INIT(1);
 static void do_read_data(struct work_struct *work);
 static void do_create_pdevs(struct work_struct *work);
 static void do_create_rpcrouter_pdev(struct work_struct *work);
+static int msm_rpcrouter_close(void);
 
 static DECLARE_WORK(work_create_pdevs, do_create_pdevs);
 static DECLARE_WORK(work_create_rpcrouter_pdev, do_create_rpcrouter_pdev);
@@ -234,8 +235,6 @@ static struct notifier_block msm_rpc_reboot_notifier = {
 	.notifier_call = msm_rpc_reboot_call,
 	.priority = 100
 };
-
-
 
 /*
  * Search for transport (xprt) that matches the provided PID.
@@ -2160,7 +2159,7 @@ int msm_rpc_get_curr_pkt_size(struct msm_rpc_endpoint *ept)
 	return rc;
 }
 
-int msm_rpcrouter_close(void)
+static int msm_rpcrouter_close(void)
 {
 	struct rpcrouter_xprt_info *xprt_info;
 	union rr_control_msg ctl;
