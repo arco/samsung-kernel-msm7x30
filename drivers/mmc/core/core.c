eea92911fb95a4214e8f1ec6ece2e0ac4fa60818
Arne Coucheron <arco68@gmail.com>
2012-10-28 00:31:32 +0200

    msm: Fix wifi sleep

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -43,6 +43,10 @@
 #include "sd_ops.h"
 #include "sdio_ops.h"
 
+#ifdef CONFIG_BCM4329
+#include "../host/msm_sdcc.h"
+#endif
+
 /*
  * The Background operations can take a long time, depends on the house keeping
  * operations the card has to perform
@@ -1472,6 +1476,8 @@ void mmc_detach_bus(struct mmc_host *host)
 
 	spin_unlock_irqrestore(&host->lock, flags);
 
+	mmc_power_off(host);
+
 	mmc_bus_put(host);
 }
 
@@ -2539,6 +2545,9 @@ int mmc_suspend_host(struct mmc_host *host)
 	if (!err && !mmc_card_keep_power(host))
 		mmc_power_off(host);
 
+	if (host->card && host->card->type == MMC_TYPE_SD)
+		mdelay(50);
+
 out:
 	return err;
 }
@@ -2602,6 +2611,9 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 {
 	struct mmc_host *host = container_of(
 		notify_block, struct mmc_host, pm_notify);
+#ifdef CONFIG_BCM4329
+	struct msmsdcc_host *msmhost = mmc_priv(host);
+#endif
 	unsigned long flags;
 
 
@@ -2653,6 +2665,13 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 		}
 		host->rescan_disable = 0;
 		spin_unlock_irqrestore(&host->lock, flags);
+
+#ifdef CONFIG_BCM4329
+		if (host->card && msmhost && msmhost->pdev_id == 1)
+			printk(KERN_INFO"%s(): WLAN SKIP DETECT CHANGE\n",
+					__func__);
+		else
+#endif
 		mmc_detect_change(host, 0);
 
 	}
