c92c902d6000d735554a387beb6c895c1f8f078b
Arne Coucheron <arco68@gmail.com>
2013-01-08 06:23:54 +0100

    ancora: Use gpio config code from stock kernel

--- arch/arm/mach-msm/board-ancora.c
+++ arch/arm/mach-msm/board-ancora.c
@@ -4263,37 +4263,30 @@ static struct msm_gpio lcdc_gpio_config_data[] = {
 	{ GPIO_CFG(129, 0, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA), "lcd_reset" },
 };
 
-/* sleep */
-static struct msm_gpio lcdc_gpio_sleep_config_data[] = {
-	{GPIO_CFG(45, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA), "spi_clk"},
-	{GPIO_CFG(46, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA), "spi_cs0"},
-	{GPIO_CFG(47, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA), "spi_mosi"},
-	{GPIO_CFG(129, 0, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_2MA), "lcd_reset"},
-};
+/* GPIO TLMM: Status */
+#define GPIO_ENABLE	0
+#define GPIO_DISABLE	1
+
+static void config_lcdc_gpio_table(uint32_t *table, int len, unsigned enable)
+{
+	int n, rc;
+
+
+	for (n = 0; n < len; n++) {
+		rc = gpio_tlmm_config(table[n],
+			enable ? GPIO_ENABLE : GPIO_DISABLE);
+		if (rc) {
+			printk(KERN_ERR "%s: gpio_tlmm_config(%#x)=%d\n",
+				__func__, table[n], rc);
+			break;
+		}
+	}
+}
 
 static void lcdc_config_gpios(int enable)
 {
-	struct msm_gpio *lcdc_gpio_cfg_data;
-	struct msm_gpio *lcdc_gpio_sleep_cfg_data;
-	int array_size;
-	int sleep_cfg_arry_size;
-
-	lcdc_gpio_cfg_data = lcdc_gpio_config_data;
-	lcdc_gpio_sleep_cfg_data = lcdc_gpio_sleep_config_data;
-	array_size = ARRAY_SIZE(lcdc_gpio_config_data);
-	sleep_cfg_arry_size = ARRAY_SIZE(lcdc_gpio_sleep_config_data);
-
-	if (enable) {
-		msm_gpios_request_enable(lcdc_gpio_cfg_data, array_size);
-	} else {
-		if (lcdc_gpio_sleep_cfg_data) {
-			msm_gpios_enable(lcdc_gpio_sleep_cfg_data, array_size);
-			msm_gpios_free(lcdc_gpio_sleep_cfg_data, array_size);
-		} else {
-			msm_gpios_disable_free(lcdc_gpio_config_data,
-					       array_size);
-		}
-	}
+	config_lcdc_gpio_table(lcdc_gpio_config_data,
+		ARRAY_SIZE(lcdc_gpio_config_data), enable);
 }
 #endif
 
@@ -4754,18 +4747,12 @@ static int lcdc_common_panel_power(int on)
 static int lcdc_panel_power(int on)
 {
 	int flag_on = !!on;
-	static int lcdc_power_save_on, lcdc_power_initialized;
+	static int lcdc_power_save_on;
 
 	if (lcdc_power_save_on == flag_on)
 		return 0;
 
 	lcdc_power_save_on = flag_on;
-
-	if (unlikely(!lcdc_power_initialized)) {
-		quickvx_mddi_client = 0;
-		display_common_init();
-		lcdc_power_initialized = 1;
-	}
 
 	return lcdc_common_panel_power(on);
 }
