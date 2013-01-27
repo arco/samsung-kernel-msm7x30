e98c6eb7bc334caeefb669ebab3652a41bc97b97
Anantha Krishnan <ananthk@codeaurora.org>
2013-01-27 18:20:20 +0530

    Bluetooth: Validate HCI device handle before updating scan state
    Check if the HCI device handle is available prior to updating
    the scan state.
    
    Signed-off-by: Anantha Krishnan <ananthk@codeaurora.org>
    
    CRs-fixed: 447893
    
    Change-Id: Ifc278afecbcf2bc7a95bb6595028dd890a8b60f5
    Signed-off-by: Fujun Wu <fujunw@codeaurora.org>

--- net/bluetooth/mgmt.c
+++ net/bluetooth/mgmt.c
@@ -2155,8 +2155,11 @@ void mgmt_inquiry_complete_evt(u16 index, u8 status)
 
 		mgmt_event(MGMT_EV_DISCOVERING, index, &cp, sizeof(cp), NULL);
 
-		if (hdev)
+		if (hdev) {
+			BT_DBG("Setting state to SCAN_IDLE\n");
+			hdev->disco_state = SCAN_IDLE;
 			goto done;
+		}
 		else
 			return;
 	}
