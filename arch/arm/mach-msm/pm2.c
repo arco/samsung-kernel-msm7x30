99aef19ef14ae415cd2ecab9850d528a831b7724
Arne Coucheron <arco68@gmail.com>
2013-02-13 00:42:25 +0100

    Revert "Remove more stuff from old kernel"
    This reverts commit 661c764787659c61f88c35360569e12fc21ab2c5.

--- arch/arm/mach-msm/pm2.c
+++ arch/arm/mach-msm/pm2.c
@@ -1730,21 +1730,111 @@ static struct platform_suspend_ops msm_pm_ops = {
 
 static uint32_t restart_reason = 0x776655AA;
 
+#if defined(CONFIG_MACH_ARIESVE) || defined(CONFIG_MACH_ANCORA) || defined(CONFIG_MACH_ANCORA_TMO) || defined(CONFIG_MACH_APACHE)
+struct smem_info {
+	unsigned int info;
+};
+
+static DEFINE_SPINLOCK(msm_reboot_lock);
+extern struct smem_info *smem_flag;
+extern void request_phone_power_off_reset(int flag);
+static void __iomem *reset_base;
+static void __iomem *msm_m_gpio2_owner_base;
+int power_off_done;
+#define RESET_ALL		0xab800200
+#define MSM_M_GPIO2_OWNER	0xabd00504
+#define RESET_ALL_VAL		0x1
+int (*set_recovery_mode)(void);
+EXPORT_SYMBOL(set_recovery_mode);
+int (*set_recovery_mode_done)(void);
+EXPORT_SYMBOL(set_recovery_mode_done);
+extern int curr_usb_status;
+extern int curr_ta_status;
+typedef struct
+{
+	unsigned int command;
+	unsigned int status;
+	unsigned int data1;
+	unsigned int data2;
+} smem_proc_comm_data_type;
+volatile smem_proc_comm_data_type *proc_comm;
+#endif
+
 static void msm_pm_power_off(void)
 {
+#if defined(CONFIG_MACH_ARIESVE) || defined(CONFIG_MACH_ANCORA) || defined(CONFIG_MACH_ANCORA_TMO) || defined(CONFIG_MACH_APACHE)
+
+	msm_rpcrouter_close();
+	proc_comm = (volatile smem_proc_comm_data_type *)MSM_SHARED_RAM_BASE;
+	proc_comm[4].command = curr_usb_status || curr_ta_status;
+	/* debug level value set to 0 to avoid entering silent reset mode when restarting */
+	smem_flag->info = 0x0;
+
+	printk("%s: send PCOM_POWER_DOWN\n",__func__);
+	msm_proc_comm(PCOM_POWER_DOWN, 0, 0);
+	power_off_done = 1;
+
+	printk("%s: call request_phone_power_off\n",__func__);
+	request_phone_power_off_reset(1);
+
+	printk("%s: do nothing!!\n",__func__);
+
+	/* block irq after machine off 2011-05-17 hc.hyun */
+	spin_lock_irq(&msm_reboot_lock);
+
+	/* if machine does not off after 30sec from PCOM_POWER_DOWN, RESET register set to 0x1 2011-05-17 hc.hyun */
+	mdelay(30000);
+	printk("%s: retry with setting PS_HOLD to low.\n",__func__);
+	writel((readl(msm_m_gpio2_owner_base) | (1<<13)), msm_m_gpio2_owner_base);
+	writel((readl(msm_m_gpio2_owner_base + 0x30) | (1<<13)), msm_m_gpio2_owner_base + 0x30);
+	gpio_set_value(29, 0);
+
+	for (;;)
+		;
+#else
 	msm_rpcrouter_close();
 	msm_proc_comm(PCOM_POWER_DOWN, 0, 0);
 	for (;;)
 		;
+#endif
 }
 
 static void msm_pm_restart(char str, const char *cmd)
 {
+#if defined(CONFIG_MACH_ARIESVE) || defined(CONFIG_MACH_ANCORA) || defined(CONFIG_MACH_ANCORA_TMO) || defined(CONFIG_MACH_APACHE)
+
+	msm_rpcrouter_close();
+
+	/* debug level value set to 0 to avoid entering silent reset mode when restarting */
+	smem_flag->info = 0x0;
+
+	printk("%s: send PCOM_RESET_CHIP\n",__func__);
+
+	msm_proc_comm(PCOM_RESET_CHIP, &restart_reason, 0);
+	//msm_proc_comm(PCOM_RESET_CHIP_IMM, &restart_reason, 0);
+	power_off_done = 1;
+
+	printk("%s: do nothing!!\n",__func__);
+
+	/* block irq after machine off 2011-05-17 hc.hyun */
+	spin_lock_irq(&msm_reboot_lock);
+
+	/* if machine does not off after 30sec from PCOM_RESET_CHIP, RESET register set to 0x1 2011-05-17 hc.hyun */
+	mdelay(30000);
+	printk("%s: retry with  setting PS_HOLD to low.\n",__func__);
+	writel((readl(msm_m_gpio2_owner_base) | (1<<13)), msm_m_gpio2_owner_base);
+	writel((readl(msm_m_gpio2_owner_base + 0x30) | (1<<13)), msm_m_gpio2_owner_base + 0x30);
+	gpio_set_value(29, 0);
+
+	for (;;)
+	;
+#else
 	msm_rpcrouter_close();
 	msm_proc_comm(PCOM_RESET_CHIP, &restart_reason, 0);
 
 	for (;;)
 		;
+#endif
 }
 
 static int msm_reboot_call
@@ -1870,6 +1960,11 @@ static int __init msm_pm_init(void)
 		d_entry->write_proc = msm_pm_write_proc;
 		d_entry->data = NULL;
 	}
+#endif
+
+#if defined(CONFIG_MACH_ARIESVE) || defined(CONFIG_MACH_ANCORA) || defined(CONFIG_MACH_ANCORA_TMO) || defined(CONFIG_MACH_APACHE)
+	reset_base = ioremap(RESET_ALL, PAGE_SIZE);
+	msm_m_gpio2_owner_base = ioremap(MSM_M_GPIO2_OWNER, PAGE_SIZE);
 #endif
 
 	return 0;
