35e7e129315d8696e0b782aa47f26d310b987787
Tirupathi Reddy <tirupath@codeaurora.org>
2013-03-08 18:08:37 +0530

    msm: restart_7k: Disable local IRQ interrupt
    While doing the power on/off test, there is a chance
    where APPS proc can receive the proc comm interrupt from
    the modem. But at this stage proc comm channels are
    already disabled and can't handle any new proc comm
    requests that are coming in. Hence disable the local
    interrupts.
    
    Change-Id: If80928e69ae0d1522b0da186e075142122d9558d
    Signed-off-by: Tirupathi Reddy <tirupath@codeaurora.org>

--- arch/arm/mach-msm/restart_7k.c
+++ arch/arm/mach-msm/restart_7k.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -60,6 +60,9 @@ volatile smem_proc_comm_data_type *proc_comm;
 
 static void msm_pm_power_off(void)
 {
+	/* Disable interrupts */
+	local_irq_disable();
+
 #if defined(CONFIG_MACH_ARIESVE) || defined(CONFIG_MACH_ANCORA) || defined(CONFIG_MACH_ANCORA_TMO) || defined(CONFIG_MACH_APACHE)
 	proc_comm = (volatile smem_proc_comm_data_type *)MSM_SHARED_RAM_BASE;
 	proc_comm[4].command = curr_usb_status || curr_ta_status;
