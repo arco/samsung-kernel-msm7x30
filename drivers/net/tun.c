01a353736add17a5847c8a3269fd0c60fb05cb31
Arne Coucheron <arco68@gmail.com>
2013-03-10 00:56:52 +0100

    net: Fix TUN problems with OpenVPN

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1245,11 +1245,9 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 	}
 #endif
 
-	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89) {
+	if (cmd == TUNSETIFF || _IOC_TYPE(cmd) == 0x89)
 		if (copy_from_user(&ifr, argp, ifreq_len))
 			return -EFAULT;
-		memset(&ifr, 0, sizeof(ifr));
-	}
 
 	if (cmd == TUNGETFEATURES) {
 		/* Currently this just means: "what IFF flags are valid?".
