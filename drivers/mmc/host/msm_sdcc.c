57a0e01bdec8466116919100c515067d32cab3ee
Konstantin Dorfman <kdorfman@codeaurora.org>
2013-03-12 15:33:53 +0200

    mmc: msm_sdcc: enable support to handle Urgent data transfer request
    In order to decrease a latency of a prioritized requests (aka Urgent
    request), we might want to stop the transmission of a running "low
    priority" request in order to handle the Urgent request.
    
    URGENT_REQUEST handling has following dependencies:
    1. Host controller driver should support mmc_host op named "stop_request"
    2. Block I/O scheduler should support re-insert API
    3. eMMC card should support HPI (High Priority Interrupt) command
    If any of the above dependencies are not met then urgent request mechanism
    will not become operational.
    
    Change-Id: I976af4fa794fb0fac701fe12f249c36d78080c6e
    Signed-off-by: Konstantin Dorfman <kdorfman@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -5938,6 +5938,7 @@ msmsdcc_probe(struct platform_device *pdev)
 	mmc->caps2 |= MMC_CAP2_SANITIZE;
 	mmc->caps2 |= MMC_CAP2_INIT_BKOPS;
 	mmc->caps2 |= MMC_CAP2_POWEROFF_NOTIFY;
+	mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
 
 	if (plat->nonremovable)
 		mmc->caps |= MMC_CAP_NONREMOVABLE;
