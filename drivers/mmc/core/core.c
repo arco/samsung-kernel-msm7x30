7215936e72a1b60e40acce4d3cb9021456989af1
Arne Coucheron <arco68@gmail.com>
2013-03-14 02:55:42 +0100

    mmc: Some changes to suspend/resume

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -40,7 +40,7 @@
 #include "sd_ops.h"
 #include "sdio_ops.h"
 
-#ifdef CONFIG_BROADCOM_WIFI
+#ifdef CONFIG_BCM4329
 #include "../host/msm_sdcc.h"
 #endif
 
@@ -1254,6 +1254,8 @@ void mmc_detach_bus(struct mmc_host *host)
 
 	spin_unlock_irqrestore(&host->lock, flags);
 
+	mmc_power_off(host);
+
 	mmc_bus_put(host);
 }
 
@@ -2095,7 +2097,7 @@ int mmc_suspend_host(struct mmc_host *host)
 	if (!err && !mmc_card_keep_power(host))
 		mmc_power_off(host);
 
-	if (!host->card || host->index == 1)
+	if (host->card && host->card->type == MMC_TYPE_SD)
 		mdelay(50);
 
 	return err;
@@ -2160,7 +2162,7 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 {
 	struct mmc_host *host = container_of(
 		notify_block, struct mmc_host, pm_notify);
-#ifdef CONFIG_BROADCOM_WIFI
+#ifdef CONFIG_BCM4329
 	struct msmsdcc_host *msmhost = mmc_priv(host);
 #endif
 	unsigned long flags;
@@ -2206,7 +2208,7 @@ int mmc_pm_notify(struct notifier_block *notify_block,
 		host->rescan_disable = 0;
 		spin_unlock_irqrestore(&host->lock, flags);
 
-#ifdef CONFIG_BROADCOM_WIFI
+#ifdef CONFIG_BCM4329
 		if (host->card && msmhost && msmhost->pdev_id == 1)
 			printk(KERN_INFO"%s(): WLAN SKIP DETECT CHANGE\n",
 					__func__);
