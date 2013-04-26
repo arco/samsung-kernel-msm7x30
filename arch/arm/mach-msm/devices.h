0afe8ef7d8a9bb92aa32032bb8b9d23b02ec76e3
Arne Coucheron <arco68@gmail.com>
2013-04-26 11:06:07 +0200

    msm: Implement support for bcmdhd wifi driver

--- arch/arm/mach-msm/devices.h
+++ arch/arm/mach-msm/devices.h
@@ -430,3 +430,5 @@ extern struct platform_device msm8930_device_acpuclk;
 extern struct platform_device msm8930aa_device_acpuclk;
 extern struct platform_device msm8960_device_acpuclk;
 extern struct platform_device msm9615_device_acpuclk;
+
+extern int brcm_wlan_init(void);
--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_custom_gpio.c
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd_custom_gpio.c
@@ -197,75 +197,6 @@ dhd_custom_get_mac_address(unsigned char *buf)
 
 /* Customized Locale table : OPTIONAL feature */
 const struct cntry_locales_custom translate_custom_table[] = {
-/* Table should be filled out based on custom platform regulatory requirement */
-#ifdef BCM4334_CHIP
-	{"",   "XZ", 11},  /* Universal if Country code is unknown or empty */
-#endif
-	{"AE", "AE", 1},
-	{"AR", "AR", 1},
-	{"AT", "AT", 1},
-	{"AU", "AU", 2},
-	{"BE", "BE", 1},
-	{"BG", "BG", 1},
-	{"BN", "BN", 1},
-	{"CA", "CA", 2},
-	{"CH", "CH", 1},
-	{"CN", "CN", 0},
-	{"CY", "CY", 1},
-	{"CZ", "CZ", 1},
-	{"DE", "DE", 3},
-	{"DK", "DK", 1},
-	{"EE", "EE", 1},
-	{"ES", "ES", 1},
-	{"FI", "FI", 1},
-	{"FR", "FR", 1},
-	{"GB", "GB", 1},
-	{"GR", "GR", 1},
-	{"HR", "HR", 1},
-	{"HU", "HU", 1},
-	{"IE", "IE", 1},
-	{"IS", "IS", 1},
-	{"IT", "IT", 1},
-	{"JP", "JP", 5},
-	{"KR", "KR", 24},
-	{"KW", "KW", 1},
-	{"LI", "LI", 1},
-	{"LT", "LT", 1},
-	{"LU", "LU", 1},
-	{"LV", "LV", 1},
-	{"MA", "MA", 1},
-	{"MT", "MT", 1},
-	{"MX", "MX", 1},
-	{"NL", "NL", 1},
-	{"NO", "NO", 1},
-	{"PL", "PL", 1},
-	{"PT", "PT", 1},
-	{"PY", "PY", 1},
-	{"RO", "RO", 1},
-	{"SE", "SE", 1},
-	{"SI", "SI", 1},
-	{"SK", "SK", 1},
-	{"TR", "TR", 7},
-	{"TW", "TW", 2},
-#ifdef CUSTOMER_HW_SAMSUNG
-	{"IR", "XZ", 11},	 /* Universal if Country code is IRAN, (ISLAMIC REPUBLIC OF) */
-	{"SD", "XZ", 11},	 /* Universal if Country code is SUDAN */
-	{"SY", "XZ", 11},	 /* Universal if Country code is SYRIAN ARAB REPUBLIC */
-	{"GL", "XZ", 11},	 /* Universal if Country code is GREENLAND */
-	{"PS", "XZ", 11},	 /* Universal if Country code is PALESTINIAN TERRITORY, OCCUPIED */
-	{"TL", "XZ", 11},	 /* Universal if Country code is TIMOR-LESTE (EAST TIMOR) */
-	{"MH", "XZ", 11},	 /* Universal if Country code is MARSHALL ISLANDS */
-	{"PK", "XZ", 11},	 /* Universal if Country code is PAKISTAN*/
-#endif
-#ifdef BCM4334_CHIP
-	{"RU", "RU", 5},
-	{"SG", "SG", 4},
-	{"US", "US", 46}
-#endif
-#ifdef BCM4330_CHIP
-	{"RU", "RU", 1},
-	{"US", "US", 5}
-#endif
 };
 
 
@@ -275,7 +206,7 @@ const struct cntry_locales_custom translate_custom_table[] = {
 */
 void get_customized_country_code(char *country_iso_code, wl_country_t *cspec)
 {
-#ifndef CUSTOMER_HW_SAMSUNG
+#if defined(CUSTOMER_HW2) && (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct cntry_locales_custom *cloc_ptr;
 
 	if (!cspec)
