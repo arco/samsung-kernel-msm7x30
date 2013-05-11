3743861a45d3739cef3d53c6bcf100b4ec033c89
Christopher <cristoforo.cataldo@gmail.com>
2013-05-11 15:11:42 +0200

    bln: Changes to fix the suspend problems caused by BLN
    Changes on bln module:
     - New centralized methods for wakelock management (init, destroy, acquire and release)
     - Some changes to call the new methods
    
    Changes on cypress-touchkey:
     - Removed the wakelock declared on this module for bln
     - Replaced the previous code for bln wakelock management with the related calls of
       the new methods created on bln module side
     - During early resume, release the possible wakelock previously acquired for bln
       and not released yet

--- include/linux/bln.h
+++ include/linux/bln.h
@@ -10,4 +10,7 @@ struct bln_implementation {
 
 void register_bln_implementation(struct bln_implementation *imp);
 bool bln_is_ongoing(void);
+void bln_wakelock_destroy(void);
+void bln_wakelock_acquire(void);
+void bln_wakelock_release(void);
 #endif
