9d9c5191edd9298f028b9df1f7046d5636e6896d
Steve Kondik <shade@chemlab.org>
2013-05-11 02:04:42 -0700

    video: msm: Call complete_all if vsync times out for LCDC
     * If vsync times out, we need to call complete_all otherwise the wait
       count goes to zero and we are stuck forever.
     * Should hopefully fix the hangs and crashes.
    
    Change-Id: Ia6673b3307251ab3ca1750d65e8c721c5c2dce5f

--- drivers/video/msm/mdp4_overlay_lcdc.c
+++ drivers/video/msm/mdp4_overlay_lcdc.c
@@ -381,6 +381,7 @@ static ssize_t vsync_show_event(struct device *dev,
 	ret = wait_for_completion_interruptible_timeout(&vctrl->vsync_comp,
 		msecs_to_jiffies(VSYNC_PERIOD * 4));
 	if (ret <= 0) {
+		complete_all(&vctrl->vsync_comp);
 		vctrl->wait_vsync_cnt = 0;
 		vctrl->vsync_time = ktime_get();
 	}
