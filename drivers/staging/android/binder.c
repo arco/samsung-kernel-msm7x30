290460d845f0962b8c382bd7463e5df5c1184a9e
Ricardo Mendoza <ricardo.mendoza@canonical.com>
2013-08-06 11:05:56 -0430

    binder: search for receiving context pid in global namespace
    BugLink: http://bugs.launchpad.net/bugs/1210149
    
    If binder cannot find the receiving task in the sender's namespace it
    will default to a null PID which thus breaks communication in our
    container-based scenario. This patch extends binder to first look in the
    senders namespace and if unsuccesfull search in the global namespace.
    
    Change-Id: I83077ffac575e632c1379bd69ea84b22acc61ba4
    Signed-off-by: Ricardo Mendoza <ricardo.mendoza@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2491,6 +2491,8 @@ retry:
 			struct task_struct *sender = t->from->proc->tsk;
 			tr.sender_pid = task_tgid_nr_ns(sender,
 							current->nsproxy->pid_ns);
+			if (tr.sender_pid == 0)
+				tr.sender_pid = task_tgid_nr(sender);
 		} else {
 			tr.sender_pid = 0;
 		}
