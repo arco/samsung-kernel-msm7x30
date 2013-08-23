1f55eef7ae28ec4b5bfcaaf1f689ca0e99227926
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-08-23 14:43:52 +0200

    Revert "qup_i2c: turn off the ahb clock conditionally."
    This reverts commit b4696c71b264f89d2c73f250c195b58a2d657921.

--- drivers/i2c/busses/i2c-qup.c
+++ drivers/i2c/busses/i2c-qup.c
@@ -328,14 +328,12 @@ qup_i2c_pwr_mgmt(struct qup_i2c_dev *dev, unsigned int state)
 	dev->clk_state = state;
 	if (state != 0) {
 		clk_enable(dev->clk);
-		if (!dev->pdata->keep_ahb_clk_on)
-			clk_enable(dev->pclk);
+		clk_enable(dev->pclk);
 	} else {
 		qup_update_state(dev, QUP_RESET_STATE);
 		clk_disable(dev->clk);
 		qup_config_core_on_en(dev);
-		if (!dev->pdata->keep_ahb_clk_on)
-			clk_disable(dev->pclk);
+		clk_disable(dev->pclk);
 	}
 }
 
@@ -1327,12 +1325,6 @@ blsp_core_init:
 	dev->clk_state = 0;
 	clk_prepare(dev->clk);
 	clk_prepare(dev->pclk);
-	/* If the same AHB clock is used on Modem side
-	 * switch it on here itself and don't switch it
-	 * on and off during suspend and resume.
-	 */
-	if (dev->pdata->keep_ahb_clk_on)
-		clk_enable(dev->pclk);
 	setup_timer(&dev->pwr_timer, qup_i2c_pwr_timer, (unsigned long) dev);
 
 	pm_runtime_set_active(&pdev->dev);
@@ -1404,11 +1396,9 @@ qup_i2c_remove(struct platform_device *pdev)
 	free_irq(dev->err_irq, dev);
 	i2c_del_adapter(&dev->adapter);
 	clk_unprepare(dev->clk);
-	if (!dev->pdata->keep_ahb_clk_on) {
-		clk_unprepare(dev->pclk);
-		clk_put(dev->pclk);
-	}
+	clk_unprepare(dev->pclk);
 	clk_put(dev->clk);
+	clk_put(dev->pclk);
 	qup_i2c_free_gpios(dev);
 	if (dev->gsbi)
 		iounmap(dev->gsbi);
@@ -1444,8 +1434,7 @@ static int qup_i2c_suspend(struct device *device)
 	if (dev->clk_state != 0)
 		qup_i2c_pwr_mgmt(dev, 0);
 	clk_unprepare(dev->clk);
-	if (!dev->pdata->keep_ahb_clk_on)
-		clk_unprepare(dev->pclk);
+	clk_unprepare(dev->pclk);
 	qup_i2c_free_gpios(dev);
 	return 0;
 }
@@ -1456,8 +1445,7 @@ static int qup_i2c_resume(struct device *device)
 	struct qup_i2c_dev *dev = platform_get_drvdata(pdev);
 	BUG_ON(qup_i2c_request_gpios(dev) != 0);
 	clk_prepare(dev->clk);
-	if (!dev->pdata->keep_ahb_clk_on)
-		clk_prepare(dev->pclk);
+	clk_prepare(dev->pclk);
 	dev->suspended = 0;
 	return 0;
 }
