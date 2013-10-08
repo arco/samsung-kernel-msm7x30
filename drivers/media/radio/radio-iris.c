8443e42b0b7c871227e9ed083d499a1c87438ac8
Ayaz Ahmad <aahmad@codeaurora.org>
2013-10-08 15:56:04 +0530

    radio: iris: Use kernel API to copy data from user space
    Use copy_from_user kernel api to copy any data from user space
    to kernel space.
    
    Change-Id: Ia3b7bb0f98180bd8792c1c18e930cb5609b8dc82
    CRs-Fixed: 540320
    Signed-off-by: Ayaz Ahmad <aahmad@codeaurora.org>

--- drivers/media/radio/radio-iris.c
+++ drivers/media/radio/radio-iris.c
@@ -3485,10 +3485,24 @@ static int iris_vidioc_s_ctrl(struct file *file, void *priv,
 		radio->riva_data_req.cmd_params.length = ctrl->value;
 		break;
 	case V4L2_CID_PRIVATE_IRIS_RIVA_POKE:
-		memcpy(radio->riva_data_req.data, (void *)ctrl->value,
-					radio->riva_data_req.cmd_params.length);
-		radio->riva_data_req.cmd_params.subopcode = RIVA_POKE_OPCODE;
-		retval = hci_poke_data(&radio->riva_data_req , radio->fm_hdev);
+		if (radio->riva_data_req.cmd_params.length <= MAX_RIVA_PEEK_RSP_SIZE) {
+			retval = copy_from_user(radio->riva_data_req.data,
+						(void *)ctrl->value,
+						radio->riva_data_req.cmd_params.length);
+			if (retval == 0) {
+				radio->riva_data_req.cmd_params.subopcode =
+									RIVA_POKE_OPCODE;
+				retval = hci_poke_data(&radio->riva_data_req,
+							radio->fm_hdev);
+			} else {
+				retval = -EINVAL;
+			}
+		} else {
+			FMDERR("Can not copy into driver's buffer. Length %d is more than"
+			 "the buffer size %d\n", radio->riva_data_req.cmd_params.length,
+				MAX_RIVA_PEEK_RSP_SIZE);
+			retval = -EINVAL;
+		}
 		break;
 	case V4L2_CID_PRIVATE_IRIS_SSBI_ACCS_ADDR:
 		radio->ssbi_data_accs.start_addr = ctrl->value;
