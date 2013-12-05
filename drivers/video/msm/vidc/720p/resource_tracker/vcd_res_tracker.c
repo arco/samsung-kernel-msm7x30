d08fecfdbf08324a2f087c279ddd32d5a17078fa
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2013-12-06 00:08:41 +0100

    msm: vidc: Fix res_trk_get_mem_type when vidc is not using ION memory

--- drivers/video/msm/vidc/720p/resource_tracker/vcd_res_tracker.c
+++ drivers/video/msm/vidc/720p/resource_tracker/vcd_res_tracker.c
@@ -746,8 +746,10 @@ struct ion_client *res_trk_get_ion_client(void)
 
 u32 res_trk_get_mem_type(void)
 {
-	u32 mem_type = ION_HEAP(resource_context.memtype);
-	return mem_type;
+	if (resource_context.vidc_platform_data->enable_ion)
+		return (u32) ION_HEAP(resource_context.memtype);
+	else
+		return resource_context.memtype;
 }
 
 void res_trk_set_mem_type(enum ddl_mem_area mem_type)
