5d506267373ae3448b1850478e5865c1115562e0
Arne Coucheron <arco68@gmail.com>
2014-01-15 02:24:48 +0100

    Revert lowmem and vmalloc patches
    Revert "mm: make is_vmalloc_addr lockless"
    
    This reverts commit 675539b2812b0db12490210ed079185f933a1b3b.
    
    Revert "Kconfig: Add menu choice option to reclaim virtual memory"
    
    This reverts commit a447197635ef394d8e9270bbcc7049f8beb20e75.
    
    Revert "Kconfig: Add config option to support vmalloc savings"
    
    This reverts commit f5130adb4c3abd7cbe977f7304bf6fb3ced84fc7.
    
    Revert "mm: Update is_vmalloc_addr to account for vmalloc savings"
    
    This reverts commit 62da64efd769329b19ca1602d5bb5c4ff564a682.
    
    Revert "msm: Increase the kernel virtual area to include lowmem"
    
    This reverts commit 274df3aaf5836c671823318d20ccfac962b4e411.
    
    Revert "msm: Allow lowmem to be non contiguous and mixed."
    
    This reverts commit 258fe02b4797883aef9823a8d1c336ada71a7516.

--- include/linux/vmalloc.h
+++ include/linux/vmalloc.h
@@ -14,7 +14,6 @@ struct vm_area_struct;		/* vma defining user mapping in mm_types.h */
 #define VM_USERMAP	0x00000008	/* suitable for remap_vmalloc_range */
 #define VM_VPAGES	0x00000010	/* buffer for pages was vmalloc'ed */
 #define VM_UNLIST	0x00000020	/* vm_struct is not listed in vmlist */
-#define VM_LOWMEM	0x00000040	/* Tracking of direct mapped lowmem */
 /* bits [20..32] reserved for arch specific ioremap internals */
 
 /*
@@ -134,13 +133,6 @@ extern rwlock_t vmlist_lock;
 extern struct vm_struct *vmlist;
 extern __init void vm_area_add_early(struct vm_struct *vm);
 extern __init void vm_area_register_early(struct vm_struct *vm, size_t align);
-extern __init int vm_area_check_early(struct vm_struct *vm);
-#ifdef CONFIG_ENABLE_VMALLOC_SAVING
-extern void mark_vmalloc_reserved_area(void *addr, unsigned long size);
-#else
-static inline void mark_vmalloc_reserved_area(void *addr, unsigned long size)
-{ };
-#endif
 
 #ifdef CONFIG_SMP
 # ifdef CONFIG_MMU
