f339b57c54edaa543265e3c8d90e066975db1d93
Arne Coucheron <arco68@gmail.com>
2014-05-16 05:30:00 +0200

    msm7x30: Increase SF memory by additional 2MB
     * Seems to cut down on out of memory situations

--- arch/arm/mach-msm/board-ariesve.c
+++ arch/arm/mach-msm/board-ariesve.c
@@ -148,7 +148,7 @@ EXPORT_SYMBOL(sec_class);
 struct device *switch_dev;
 EXPORT_SYMBOL(switch_dev);
 
-#define MSM_PMEM_SF_SIZE	0x1A00000
+#define MSM_PMEM_SF_SIZE	0x1C00000
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_PRIM_BUF_SIZE	(roundup((800 * 480 * 4), 4096) * 3) /* 4bpp * 3 Pages */
 #else
