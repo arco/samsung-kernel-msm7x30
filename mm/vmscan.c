77f2f8a82f787e485d5f4cb55c39965c0065d324
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-06-08 13:52:52 +0200

    mm: vmscan: Fix build error

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2116,7 +2116,7 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 		count_vm_event(ALLOCSTALL);
 
 	do {
-		vmpressure_prio(sc->gfp_mask, sc->target_mem_cgroup, priority);
+		vmpressure_prio(sc->gfp_mask, sc->target_mem_cgroup, sc->priority);
 		sc->nr_scanned = 0;
 		aborted_reclaim = shrink_zones(zonelist, sc);
 
