d5600570f4b88e2aefa8935034b3c11f89257fab
Arne Coucheron <arco68@gmail.com>
2014-06-16 05:42:51 +0200

    ancora: Adjust PMEM sizes

--- arch/arm/mach-msm/board-ancora.c
+++ arch/arm/mach-msm/board-ancora.c
@@ -152,7 +152,7 @@ EXPORT_SYMBOL(sec_class);
 struct device *switch_dev;
 EXPORT_SYMBOL(switch_dev);
 
-#define MSM_PMEM_SF_SIZE	0x1D00000
+#define MSM_PMEM_SF_SIZE	0x1C00000
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MSM_FB_PRIM_BUF_SIZE	(roundup((800 * 480 * 4), 4096) * 3) /* 4bpp * 3 Pages */
 #else
@@ -161,7 +161,7 @@ EXPORT_SYMBOL(switch_dev);
 
 #define MSM_FB_SIZE roundup(MSM_FB_PRIM_BUF_SIZE, 4096)
 
-#define MSM_PMEM_ADSP_SIZE		0x1400000
+#define MSM_PMEM_ADSP_SIZE		0x1A00000
 #define MSM_FLUID_PMEM_ADSP_SIZE	0x2800000
 #define PMEM_KERNEL_EBI0_SIZE		0x0600000
 #define MSM_PMEM_AUDIO_SIZE		0x0200000
