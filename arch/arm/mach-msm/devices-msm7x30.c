0bc0a50e37af818fda110f62bc8464349085332b
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-06-22 16:31:50 +0300

    msm7x30: Disable unsupported features
    These parameters are only handled on 1080p.

--- arch/arm/mach-msm/devices-msm7x30.c
+++ arch/arm/mach-msm/devices-msm7x30.c
@@ -1002,8 +1002,7 @@ static struct resource msm_vidc_720p_resources[] = {
 struct msm_vidc_platform_data vidc_platform_data = {
 	.memtype = ION_CP_MM_HEAP_ID,
 	.enable_ion = 1,
-	.disable_dmx = 0,
-	.cont_mode_dpb_count = 8
+	.disable_dmx = 1
 };
 
 struct platform_device msm_device_vidc_720p = {
