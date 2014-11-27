016e463ea4ff995c3eb30367d47f6dab572efa5f
Arne Coucheron <arco68@gmail.com>
2014-11-27 05:24:40 +0100

    broadcom_wlan: Modify country code source

--- arch/arm/mach-msm/broadcom_wlan.c
+++ arch/arm/mach-msm/broadcom_wlan.c
@@ -116,71 +116,183 @@ static int brcm_init_wlan_mem(void)
 }
 
 /* Customized Locale table : OPTIONAL feature */
-#define WLC_CNTRY_BUF_SZ        4
-typedef struct cntry_locales_custom {
-	char iso_abbrev[WLC_CNTRY_BUF_SZ];
-	char custom_locale[WLC_CNTRY_BUF_SZ];
+#define COUNTRY_BUF_SZ        4
+struct cntry_locales_custom {
+	char iso_abbrev[COUNTRY_BUF_SZ];
+	char custom_locale[COUNTRY_BUF_SZ];
 	int  custom_locale_rev;
-} cntry_locales_custom_t;
+};
 
-static cntry_locales_custom_t brcm_wlan_translate_custom_table[] = {
+static struct cntry_locales_custom brcm_wlan_translate_custom_table[] = {
 /* Table should be filled out based on custom platform regulatory requirement */
-	{"",   "XY", 4},  /* universal */
-	{"US", "US", 69}, /* input ISO "US" to : US regrev 69 */
-	{"CA", "US", 69}, /* input ISO "CA" to : US regrev 69 */
-	{"EU", "EU", 5},  /* European union countries */
-	{"AT", "EU", 5},
-	{"BE", "EU", 5},
-	{"BG", "EU", 5},
-	{"CY", "EU", 5},
-	{"CZ", "EU", 5},
-	{"DK", "EU", 5},
-	{"EE", "EU", 5},
-	{"FI", "EU", 5},
-	{"FR", "EU", 5},
-	{"DE", "EU", 5},
-	{"GR", "EU", 5},
-	{"HU", "EU", 5},
-	{"IE", "EU", 5},
-	{"IT", "EU", 5},
-	{"LV", "EU", 5},
-	{"LI", "EU", 5},
-	{"LT", "EU", 5},
-	{"LU", "EU", 5},
-	{"MT", "EU", 5},
-	{"NL", "EU", 5},
-	{"PL", "EU", 5},
-	{"PT", "EU", 5},
-	{"RO", "EU", 5},
-	{"SK", "EU", 5},
-	{"SI", "EU", 5},
-	{"ES", "EU", 5},
-	{"SE", "EU", 5},
-	{"GB", "EU", 5},  /* input ISO "GB" to : EU regrev 05 */
-	{"IL", "IL", 0},
-	{"CH", "CH", 0},
-	{"TR", "TR", 0},
-	{"NO", "NO", 0},
-	{"KR", "XY", 3},
-	{"AU", "XY", 3},
-	{"CN", "XY", 3},  /* input ISO "CN" to : XY regrev 03 */
-	{"TW", "XY", 3},
-	{"AR", "XY", 3},
-	{"MX", "XY", 3}
+	{"",   "XV", 17},	/* Universal if Country code is unknown or empty */
+	{"IR", "XV", 17},	/* Universal if Country code is IRAN, (ISLAMIC REPUBLIC OF) */
+	{"SD", "XV", 17},	/* Universal if Country code is SUDAN */
+	{"SY", "XV", 17},	/* Universal if Country code is SYRIAN ARAB REPUBLIC */
+	{"GL", "XV", 17},	/* Universal if Country code is GREENLAND */
+	{"PS", "XV", 17},	/* Universal if Country code is PALESTINE */
+	{"TL", "XV", 17},	/* Universal if Country code is TIMOR-LESTE (EAST TIMOR) */
+	{"MH", "XV", 17},	/* Universal if Country code is MARSHALL ISLANDS */
+	{"PK", "XV", 17},	/* Universal if Country code is PAKISTAN */
+	{"CK", "XV", 17},	/* Universal if Country code is Cook Island (13.4.27)*/
+	{"CU", "XV", 17},	/* Universal if Country code is Cuba (13.4.27)*/
+	{"FK", "XV", 17},	/* Universal if Country code is Falkland Island (13.4.27)*/
+	{"FO", "XV", 17},	/* Universal if Country code is Faroe Island (13.4.27)*/
+	{"GI", "XV", 17},	/* Universal if Country code is Gibraltar (13.4.27)*/
+	{"IM", "XV", 17},	/* Universal if Country code is Isle of Man (13.4.27)*/
+	{"CI", "XV", 17},	/* Universal if Country code is Ivory Coast (13.4.27)*/
+	{"JE", "XV", 17},	/* Universal if Country code is Jersey (13.4.27)*/
+	{"KP", "XV", 17},	/* Universal if Country code is North Korea (13.4.27)*/
+	{"FM", "XV", 17},	/* Universal if Country code is Micronesia (13.4.27)*/
+	{"MM", "XV", 17},	/* Universal if Country code is Myanmar (13.4.27)*/
+	{"NU", "XV", 17},	/* Universal if Country code is Niue (13.4.27)*/
+	{"NF", "XV", 17},	/* Universal if Country code is Norfolk Island (13.4.27)*/
+	{"PN", "XV", 17},	/* Universal if Country code is Pitcairn Islands (13.4.27)*/
+	{"PM", "XV", 17},	/* Universal if Country code is Saint Pierre and Miquelon (13.4.27)*/
+	{"SS", "XV", 17},	/* Universal if Country code is South_Sudan (13.4.27)*/
+	{"AL", "AL", 2},
+	{"DZ", "DZ", 1},
+	{"AS", "AS", 12},	/* changed 2 -> 12*/
+	{"AI", "AI", 1},
+	{"AG", "AG", 2},
+	{"AR", "AR", 21},
+	{"AW", "AW", 2},
+	{"AU", "AU", 6},
+	{"AT", "AT", 4},
+	{"AZ", "AZ", 2},
+	{"BS", "BS", 2},
+	{"BH", "BH", 4},	/* changed 24 -> 4*/
+	{"BD", "BD", 2},
+	{"BY", "BY", 3},
+	{"BE", "BE", 4},
+	{"BM", "BM", 12},
+	{"BA", "BA", 2},
+	{"BR", "BR", 4},
+	{"VG", "VG", 2},
+	{"BN", "BN", 4},
+	{"BG", "BG", 4},
+	{"KH", "KH", 2},
+	{"CA", "CA", 31},
+	{"KY", "KY", 3},
+	{"CN", "CN", 24},
+	{"CO", "CO", 17},
+	{"CR", "CR", 17},
+	{"HR", "HR", 4},
+	{"CY", "CY", 4},
+	{"CZ", "CZ", 4},
+	{"DK", "DK", 4},
+	{"EE", "EE", 4},
+	{"ET", "ET", 2},
+	{"FI", "FI", 4},
+	{"FR", "FR", 5},
+	{"GF", "GF", 2},
+	{"DE", "DE", 7},
+	{"GR", "GR", 4},
+	{"GD", "GD", 2},
+	{"GP", "GP", 2},
+	{"GU", "GU", 12},
+	{"HK", "HK", 2},
+	{"HU", "HU", 4},
+	{"IS", "IS", 4},
+	{"IN", "IN", 3},
+	{"ID", "KR", 25},	/* ID/1 -> KR/24 */
+	{"IE", "IE", 5},
+	{"IL", "BO", 0},	/* IL/7 -> BO/0 */
+	{"IT", "IT", 4},
+	{"JP", "JP", 58},
+	{"JO", "JO", 3},
+	{"KW", "KW", 5},
+	{"LA", "LA", 2},
+	{"LV", "LV", 4},
+	{"LB", "LB", 5},
+	{"LS", "LS", 2},
+	{"LI", "LI", 4},
+	{"LT", "LT", 4},
+	{"LU", "LU", 3},
+	{"MO", "MO", 2},
+	{"MK", "MK", 2},
+	{"MW", "MW", 1},
+	{"MY", "MY", 3},
+	{"MV", "MV", 3},
+	{"MT", "MT", 4},
+	{"MQ", "MQ", 2},
+	{"MR", "MR", 2},
+	{"MU", "MU", 2},
+	{"YT", "YT", 2},
+	{"MX", "MX", 20},
+	{"MD", "MD", 2},
+	{"MC", "MC", 1},
+	{"ME", "ME", 2},
+	{"MA", "MA", 2},
+	{"NP", "NP", 3},
+	{"NL", "NL", 4},
+	{"AN", "AN", 2},
+	{"NZ", "NZ", 4},
+	{"NO", "NO", 4},
+	{"OM", "OM", 4},
+	{"PA", "PA", 17},
+	{"PG", "PG", 2},
+	{"PY", "PY", 2},
+	{"PE", "PE", 20},
+	{"PH", "PH", 5},
+	{"PL", "PL", 4},
+	{"PT", "PT", 4},
+	{"PR", "PR", 20},
+	{"RE", "RE", 2},
+	{"RO", "RO", 4},
+	{"SN", "SN", 2},
+	{"RS", "RS", 2},
+	{"SG", "SG", 4},
+	{"SK", "SK", 4},
+	{"SI", "SI", 4},
+	{"ES", "ES", 4},
+	{"LK", "LK", 1},
+	{"SE", "SE", 4},
+	{"CH", "CH", 4},
+	{"TW", "TW", 1},
+	{"TH", "TH", 5},
+	{"TT", "TT", 3},
+	{"TR", "TR", 7},
+	{"AE", "AE", 6},
+	{"UG", "UG", 2},
+	{"GB", "GB", 6},
+	{"UY", "UY", 1},
+	{"VI", "VI", 13},
+	{"VA", "VA", 2},
+	{"VE", "VE", 3},
+	{"VN", "VN", 4},
+	{"MA", "MA", 1},
+	{"ZM", "ZM", 2},
+	{"EC", "EC", 21},
+	{"SV", "SV", 19},
+	{"KR", "KR", 57},
+	{"RU", "RU", 13},
+	{"UA", "UA", 8},
+	{"GT", "GT", 1},
+	{"MN", "MN", 1},
+	{"NI", "NI", 2},
+	{"US", "Q2", 57},
 };
 
 static void *brcm_wlan_get_country_code(char *ccode)
 {
-	int size = ARRAY_SIZE(brcm_wlan_translate_custom_table);
-	int i;
+	int size, i;
+	static struct cntry_locales_custom country_code;
 
-	if (!ccode)
+	size = ARRAY_SIZE(brcm_wlan_translate_custom_table);
+
+	if ((size == 0) || (ccode == NULL))
 		return NULL;
 
-	for (i = 0; i < size; i++)
-		if (strcmp(ccode, brcm_wlan_translate_custom_table[i].iso_abbrev) == 0)
+	for (i = 0; i < size; i++) {
+		if (!strcmp(ccode, brcm_wlan_translate_custom_table[i].iso_abbrev))
 			return &brcm_wlan_translate_custom_table[i];
-	return &brcm_wlan_translate_custom_table[0];
+	}
+
+	memset(&country_code, 0, sizeof(struct cntry_locales_custom));
+	strlcpy(country_code.custom_locale, ccode, COUNTRY_BUF_SZ);
+
+	return &country_code;
 }
 
 static int wlan_wifi_cd = 0;
@@ -305,7 +417,7 @@ int brcm_wlan_reset(int onoff)
 
 static struct resource brcm_wlan_resources[] = {
 	[0] = {
-		.name   = "bcm4329_wlan_irq",
+		.name   = "bcmdhd_wlan_irq",
 		.start  = MSM_GPIO_TO_INT(WLAN_HOST_WAKE),
 		.end    = MSM_GPIO_TO_INT(WLAN_HOST_WAKE),
 		.flags  = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
@@ -323,7 +435,7 @@ static struct wifi_platform_data brcm_wlan_control = {
 };
 
 static struct platform_device brcm_device_wlan = {
-	.name			= "bcm4329_wlan",
+	.name			= "bcmdhd_wlan",
 	.id			= 1,
 	.num_resources		= ARRAY_SIZE(brcm_wlan_resources),
 	.resource		= brcm_wlan_resources,
